72f239d22207b160599a6604e1275e17f488d212
samiahmed90 <mneykicks7@gmail.com>
2026-06-15 01:48:17 +0600

    Wrote the generate_key Function

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello world")
+from cryptography.fernet import Fernet
+
+def generate_key(file_path = 'encryption_key.key'):
+    key = Fernet.generate_key()
+    with open (file_path, 'wb') as key_file:
+        key_file.write(key)
+        print (f"Encryption key is saved in to {file_path}")
+
+generate_key()
